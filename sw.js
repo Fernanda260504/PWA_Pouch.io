44247c9c27eac767339e3bc2f7e3551197a9c351
Miguel Sotelo <20223TN113@utez.edu.mx>
2025-11-06 10:58:06 -0600

    Initial commit: Proyecto PWA listo

---
+++ sw.js
@@ -0,0 +1,31 @@
+const cacheName = 'offline-cache-v1';
+
+const WC = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/pouchdb@9.0.0/dist/pouchdb.min.js',]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(
+                    WC
+                );
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => {
+                    return caches.match(event.request);
+                })
+        )
+    }
+})
